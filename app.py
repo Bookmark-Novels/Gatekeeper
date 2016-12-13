ab1d71e9e32139e3c57ccb65b90f695216957fd2
Andrew Lee <andrew.dragon.lee@gmail.com>
2016-12-13 18:17:44 -0500

    change css a bit, remove password verify from registration, no csrf protection in dev environments, added instance config file containing application instance information for logging etc

--- app.py
+++ app.py
@@ -12,12 +12,29 @@ from modules.secrets import secrets
 app = Flask('bookmark')
 
 app.secret_key = secrets.gatekeeper_secret
+app.server_name = secrets.gatekeeper_host
 app.session_cookie_secure = True
 
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@app.context_processor
+def injections():
+    to_inject = {}
+    
+    if secrets.DEBUG:
+        to_inject['csrf_token'] = lambda: 0
+
+    return to_inject
+
+# Enabling CSRF protection seems to break
+# automatic reloads on template changes.
+#
+# Do not enable protection in dev environments.
+if not secrets.DEBUG:
+    CsrfProtect(app)
 
 @app.before_request
 def pre_request():
@@ -31,5 +48,5 @@ def post_request(response):
 from gatekeeper import *
 
 if __name__ == '__main__':
-    CsrfProtect(app)
+
     app.run(port=secrets.port, debug=secrets.DEBUG)
